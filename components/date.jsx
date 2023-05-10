beae1b424fee70f53c5659fae854fee270f1a067
alexyxwei <alexyxwei@tencent.com>
2023-05-10 16:31:22 +0800

    chore: change date-fns to dayjs

--- components/date.jsx
+++ components/date.jsx
@@ -1,6 +1,9 @@
-import { parseISO, format } from 'date-fns'
+import dayjs from 'dayjs'
 
 export default function Date({ dateString }) {
-  const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
+  return (
+    <time dateTime={dateString}>
+      {dayjs(dateString).format('MMMM DD, yyyy')}
+    </time>
+  )
 }
